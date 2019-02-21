7e333ffe63ddc6a3e14e50afe65144c34c6a5c22
Josh Bodah <jbodah@cargurus.com>
2019-02-21 13:16:21 -0500

    fix spy_rb load

--- lib/jb_tools/gem_development.rb
+++ lib/jb_tools/gem_development.rb
@@ -3,7 +3,7 @@ require "jb_tools/gem_development/version"
 require "minitest/pride"
 require "minitest/spec"
 require "minitest/tagz/focus"
-require "spy_rb"
+require "spy"
 require "pry"
 
 module JbTools
